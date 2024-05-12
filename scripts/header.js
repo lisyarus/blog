4a40575856efa3f97eb0994f2fcfd14b89755d24
lisyarus <lisyarus@gmail.com>
2024-05-12 23:13:57 +0300

    Update footer

--- scripts/header.js
+++ scripts/header.js
@@ -28,7 +28,7 @@ let endSectionHTML = `
 <hr class="header-separator">
 <p>Hey, if you like my articles, consider supporting my other work!</p>
 <p>For example, watch my <a href="https://youtube.com/@lisyarus">YouTube devlogs</a>, like this one:</p>
-<center><iframe src="https://www.youtube.com/embed/2DguAOHpZ8c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="youtube-embed"></iframe></center>
+<center><iframe src="https://www.youtube.com/embed/iE3p30PfiVI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="youtube-embed"></iframe></center>
 `;
 
 document.getElementById("blog-header").innerHTML = headerHTML;
